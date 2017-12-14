54ab4988a92d6415a529e9f3df0f0c7eb388a4d9
zhzhenqin <zhzhenqin@163.com>
2017-12-14 15:10:15 +0800

    修复打包逻辑，去掉不需要的打包

---
+++ katta-client/src/test/java/com/ivyft/katta/node/KattaNode.java
@@ -0,0 +1,25 @@
+package com.ivyft.katta.node;
+
+import com.ivyft.katta.Katta;
+
+/**
+ * <pre>
+ *
+ * Created by zhenqin.
+ * User: zhenqin
+ * Date: 17/12/14
+ * Time: 12:41
+ * Vendor: yiidata.com
+ * To change this template use File | Settings | File Templates.
+ *
+ * </pre>
+ *
+ * @author zhenqin
+ */
+public class KattaNode {
+
+
+    public static void main(String[] args) throws Exception {
+        Katta.main(new String[]{"node", "-s"});
+    }
+}
